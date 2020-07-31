c4dec81b8dbe16023f9212366aabfad43171dcf9
Samuel Soares Guimarães <samuel.soares@unidesk.com.br>
2020-07-31 16:14:39 -0300

    Add project files.

---
+++ CalculoFGTS/MascaraMoeda.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalculoFGTS
+{
+    internal class MascaraMoeda : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(entry);
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(entry);
+        }
+
+        private static void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            if (!string.IsNullOrWhiteSpace(args.NewTextValue))
+            {
+                bool ehCallback = false;
+                string texto = args.NewTextValue;
+
+                if (texto.Contains("R$"))
+                {
+                    var valorNovoEmDecimal = ConverterReaisParaDecimal(texto);
+                    var valorAntigoEmDecimal = args.OldTextValue.Contains("R$") ? ConverterReaisParaDecimal(args.OldTextValue) : int.Parse(args.OldTextValue);
+                    ehCallback = valorNovoEmDecimal == valorAntigoEmDecimal;
+
+                    texto = valorNovoEmDecimal.ToString();
+                }
+
+                if (!ehCallback)
+                {
+                    if (!string.IsNullOrEmpty(texto))
+                    {
+                        var textoFormatadoEmReais = (Decimal.Parse(texto) / 100).ToString("C", CultureInfo.GetCultureInfo("pt-BR"));
+                        texto = textoFormatadoEmReais;
+                    }
+
+                    ((Entry)sender).Text = texto;
+                }
+            }
+        }
+
+        private static int ConverterReaisParaDecimal(string valor)
+        {
+            var valorConvertido = Decimal.Parse(valor.Replace("R$ ", "").Replace(",", "").Replace(".", ""),
+                CultureInfo.GetCultureInfo("pt-BR"));
+            return (int)valorConvertido;
+        }
+    }
+}
---
+++ CalculoFGTS/Views/ItemsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using CalculoFGTS.Views;
+
+namespace CalculoFGTS.Views
+{
+    [DesignTimeVisible(false)]
+    public partial class ItemsPage : ContentPage
+    {
+        public ItemsPage()
+        {
+            InitializeComponent();
+            BOTAO.Clicked += CalcularImpostoRenda;
+        }
+
+        private void CalcularImpostoRenda(object sender, EventArgs args)
+        {
+            double valor = Convert.ToDouble(this.VALOR.Text.Replace("R$", ""));
+
+            double valorTotal = (valor * 8) / 100;
+
+            DisplayAlert("INFORMAÇÃO", "Valor de FGTS : R$" + valorTotal.ToString() + ",00", "OK");
+        }
+    }
+}
